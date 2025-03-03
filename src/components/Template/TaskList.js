478ce313180cfef25fe6713a47e0679944d3a032
ThanhLuan1903 <tranthanhluan1903@gmail.com>
2025-03-03 23:07:22 +0700

    feature: handle API sync template tasks to personal task

--- src/components/Template/TaskList.js
+++ src/components/Template/TaskList.js
@@ -1,9 +1,38 @@
+import { useState } from "react";
 import { Button } from "../ui";
+import { createTaskFromTemplate } from "../../api/template";
+import ToastMessage from "../ui/ToastMessage";
+import { site_path } from "../../utils";
+import { useNavigate } from "react-router-dom";
 
 export function TaskList({ tasks }) {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(null);
+  const [toast, setToast] = useState(null);
+
   const formatTime = (isoString) => {
     const date = new Date(isoString);
-    return date.toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" });
+    return date.toLocaleTimeString("en-GB", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
+  const handleApply = async (templateId) => {
+    setLoading(templateId);
+    try {
+      await createTaskFromTemplate(templateId);
+      setToast({ type: "success", message: "Sync successful!" });
+
+      setTimeout(() => {
+        setToast(null);
+        navigate(site_path.TASKSTATUS);
+      }, 2000);
+    } catch (error) {
+      setToast({ type: "error", message: "Sync failed." });
+    } finally {
+      setLoading(null);
+    }
   };
 
   return (
@@ -11,7 +40,7 @@ export function TaskList({ tasks }) {
       {tasks.map((template) => (
         <div key={template.id} className="mb-4">
           <h2 className="font-bold text-lg mb-2">{template.title}</h2>
-          
+
           <div className="border border-orange rounded-md p-3 grid grid-cols-2 gap-4">
             {template.tasks.length > 0 ? (
               [0, 1].map((colIndex) => (
@@ -19,25 +48,43 @@ export function TaskList({ tasks }) {
                   {template.tasks
                     .filter((_, index) => index % 2 === colIndex)
                     .map((task) => (
-                      <div key={task.id} className="p-2 rounded-md text-sm bg-[#D6EFD8]">
-                        {/* Hiển thị thời gian + title */}
+                      <div
+                        key={task.id}
+                        className="p-2 rounded-md text-sm bg-[#D6EFD8]"
+                      >
                         <p className="font-sm">
-                          {formatTime(task.startTime)} - {formatTime(task.endTime)} {task.title}
+                          {formatTime(task.startTime)} -{" "}
+                          {formatTime(task.endTime)} {task.title}
                         </p>
                       </div>
                     ))}
                 </div>
               ))
             ) : (
-              <p className="text-gray-500 col-span-2 text-center">No tasks available</p>
+              <p className="text-gray-500 col-span-2 text-center">
+                No tasks available
+              </p>
             )}
           </div>
-      <Button size="sm" type="submit" className="w-1/4 mt-2">
-        Apply
-      </Button>
+
+          <Button
+            size="sm"
+            type="submit"
+            className="w-1/4 mt-2"
+            onClick={() => handleApply(template.id)}
+            disabled={loading === template.id}
+          >
+            {loading === template.id ? "Applying..." : "Apply"}
+          </Button>
+          {toast && (
+            <ToastMessage
+              type={toast.type}
+              message={toast.message}
+              bottomOffset={100}
+            />
+          )}
         </div>
       ))}
-
     </>
   );
 }
